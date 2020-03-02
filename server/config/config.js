7e970e187fd5ee4417880385ac1bec58f1f74e3b
GeorgeBeta <jorge@zaporta.com>
2020-03-02 21:59:31 +0100

    Antes reordenar Routes

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,11 @@
 //  PUERTO
 //
 process.env.PORT = process.env.PORT || 3000;
+
+//
+//  AWS RDS - MySQL
+//
+process.env.RDS_HOSTNAME = process.env.RDS_HOSTNAME || 'zaporlentz.cks454fz2vcw.eu-west-1.rds.amazonaws.com';
+process.env.RDS_USERNAME = process.env.RDS_USERNAME || 'admin';
+process.env.RDS_PASSWORD = process.env.RDS_PASSWORD || 'Carla470998';
+process.env.RDS_PORT = process.env.RDS_PORT || 3306;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const bodyPaser = require('body-parser');
+const mysql = require('mysql');
 
 const app = express();
 require('./config/config');
 app.use(bodyPaser.urlencoded({ extended: false }));
+
+// Conectar la Badse de datos
+const connection = mysql.createConnection({
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Conexión a la base de datos fallida: ' + err.stack);
+        return;
+    }
+    connection.query('CREATE DATABASE IF NOT EXISTS egoHouse;');
+    connection.query('USE egoHouse;');
+    let cadena = 'CREATE TABLE IF NOT EXISTS usuario(id int NOT NULL AUTO_INCREMENT, ' +
+        'usuario varchar(30), ' +
+        'password varchar(255), ' +
+        'email varchar(255), ' +
+        'role varchar(10), ' +
+        'estado boolean, ' +
+        'fechaAlta datetime, ' +
+        'PRIMARY KEY(id));';
+    console.log(cadena);
+    connection.query(cadena, function(error, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
+    console.log('Base de Datos Conectada.');
+    connection.end();
+});
+
 
 app.get('/', function(req, res) {
     res.json('Hola Reservas');
